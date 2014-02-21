a30f376d9db4b4f2d2c3fff499c7333b5bd2968f
fatima <fatima@129.64.235.62>
2014-02-21 01:39:47 -0500

    add user form ,remove the random button

--- FishGame-android/src/com/me/FishGame/MainActivity.java
+++ FishGame-android/src/com/me/FishGame/MainActivity.java
@@ -1,18 +1,22 @@
-package com.me.FishGame;
 
-import android.os.Bundle;
-import android.view.WindowManager;
 
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+	package com.me.FishGame;
 
-public class MainActivity extends AndroidApplication {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);// to make the screen active all the time
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
-        initialize(new FishGame(), cfg);
-    }
-}
+	import android.os.Bundle;
+	import android.view.WindowManager;
+
+	import com.badlogic.gdx.backends.android.AndroidApplication;
+	import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+	public class MainActivity extends AndroidApplication {
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);// to make the screen active all the time
+	        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+	        cfg.useGL20 = false;
+	        initialize(new FishGame(), cfg);
+	    }
+	}
+
+
---
+++ FishGame-android/src/com/me/FishGame/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.me.FishGame;
+
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.VideoView;
+
+public class SplashScreen extends Activity implements OnCompletionListener
+{ 
+	 
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		// to make the screen active all the time
+	     
+		 getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);   
+		String fileName = "android.resource://"+  getPackageName() +"/raw/video";
+		VideoView vv = (VideoView) this.findViewById(R.id.surface);
+		vv.setVideoURI(Uri.parse(fileName));
+		vv.setOnCompletionListener(this);
+		vv.start();
+
+	}
+
+
+	public void onCompletion(MediaPlayer mp) {
+		// TODO Auto-generated method stub
+		
+		Intent intent = new Intent(this, MainActivity.class);
+		
+		startActivity(intent);      
+		finish();
+		
+	}
+}
